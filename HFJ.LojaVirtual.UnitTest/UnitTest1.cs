58c240ff3eee54e230f47577a94f4a1f1d6a8a8c
Marcelo Junqueira <marcedoj@hotmail.com>
2016-03-29 14:29:15 -0300

    Envio inicial

---
+++ HFJ.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HFJ.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestHFJ
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ HFJ.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HFJ.LojaVirtual.Web.MvcApplication" Language="C#" %>
